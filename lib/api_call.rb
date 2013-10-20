245c208d53e07d7049781dc1b012a001605326b7
Clare Glinka <glinka.cb@gmail.com>
2013-10-20 13:41:28 -0500

    Add brief comments to ApiCall::Call methods.

--- lib/api_call.rb
+++ lib/api_call.rb
@@ -6,18 +6,24 @@ module ApiCall
   class Call
     attr_accessor :title
 
+    # Take argument title and set it as instance variable.
+    #
     def initialize(title)
       @title = title
       @action = 'query'
       @prop = 'extracts'
     end
 
+    # Make a string that is the URL for the API call.
+    #
     def form_string
       @base = "http://en.wikipedia.org/w/api.php?action=#{@action}&prop=#{@prop}&format=json"
       @title = URI::encode(@title)
       @base + '&titles=' + @title
     end
 
+    # Call the API and parse the returning JSON object.
+    #
     def call_api
       JSON.parse(RestClient.get form_string)
     end
