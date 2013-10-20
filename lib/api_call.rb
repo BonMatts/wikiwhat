2f107d782f664695847c4b0b053bc2e93147e871
Clare Glinka <glinka.cb@gmail.com>
2013-10-20 12:47:26 -0500

    Add tests for ApiCall, two passing methods for ApiCall::Call.

--- lib/api_call.rb
+++ lib/api_call.rb
@@ -1,6 +1,24 @@
+require 'open-uri'
+require 'RestClient'
+
 module ApiCall
   class Call
-    def initialize
+    attr_accessor :title
+
+    def initialize(title)
+      @title = title
+      @action = 'query'
+      @prop = 'extracts'
+    end
+
+    def form_string
+      @base = "http://en.wikipedia.org/w/api.php?action=#{@action}&prop=#{@prop}&format=json"
+      @title = URI::encode(@title)
+      @base + '&titles=' + @title
+    end
+
+    def call_api
+      
     end
   end
 end
--- spec/api_call_spec.rb
+++ spec/api_call_spec.rb
@@ -1,8 +1,33 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'rspec'
+require 'vcr'
+require 'json'
+require 
 require_relative '../lib/api_call'
+require_relative 'vcr_setup.rb'
+require_relative 'einstein_extract_full_text_json.rb'
 
-describe ApiCall do 
-  
+describe ApiCall::Call do
+  context 'User requests inital paragraph of wiki article' do
+    let(:search) { ApiCall::Call.new('Albert Einstein') }
+
+    describe '.initialize' do
+      it 'sets the title variable' do
+        expect(search.title).to eq('Albert Einstein')
+      end
+    end
+
+    describe '#form_string' do
+      it 'forms url string with correct parameters' do
+        expect(search.form_string).to eq('http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&titles=Albert%20Einstein')
+      end
+    end
+
+    describe '#call_api' do
+      it 'uses RestClient to make API call' do
+        expect(search.call_api).to eq(  )
+      end
+    end
+  end
 end
