b07a9f2ac3ed075990b33d4ae7e6abf2ed14969b
Clare Glinka <glinka.cb@gmail.com>
2013-10-20 14:00:07 -0500

    Update spec/testfiles.

--- spec/testfiles/api_test_einstein.rb
+++ spec/testfiles/api_test_einstein.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'rest_client'
 
 
+# TODO: stub out later with VCR
 def testoutput
-JSON.parse(RestClient.get "http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&titles=Albert%20Einstein")
+  JSON.parse(RestClient.get "http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&titles=Albert%20Einstein")
 end
