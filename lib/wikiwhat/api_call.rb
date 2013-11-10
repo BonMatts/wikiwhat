5d536ab4a3458ae01edc82a8d6fd6c715f9c3acb
Bonnie <blmattson@gmail.com>
2013-11-09 22:30:17 -0600

    add geosearch to options in api_call.rb

--- lib/wikiwhat/api_call.rb
+++ lib/wikiwhat/api_call.rb
@@ -12,8 +12,11 @@ module Wikiwhat
       options[:rvprop] ? rvprop = "&rvprop=content" : rvprop = ''
       options[:img_list] ? img_list = "&generator=images" : img_list = ''
       options[:iiprop] ? iiprop = "&iiprop=url" : iiprop = ''
+      options[:geosearch] ? geosearch = "&list=geosearch&gsradius=10000&gscoord=#{options[:coordinates]}"
 
-      JSON.parse(RestClient.get "http://en.wikipedia.org/w/api.php?action=query#{prop}&titles=#{title}&format=json&redirects#{img_list}#{rvprop}#{iiprop}")
+      list=geosearch&gsradius=10000&gscoord=37.786971|-122.399677
+
+      JSON.parse(RestClient.get "http://en.wikipedia.org/w/api.php?action=query#{prop}&titles=#{title}&format=json&redirects#{img_list}#{rvprop}#{iiprop}#{geosearch}")
     end
   end
 end
